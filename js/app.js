4d8756b29f34f6e3f0913dd39ae5879f02ec232f
VERONICAHR27 <hilarioreyesveronica6@gmail.com>
2024-06-18 21:50:05 -0500

    Resolviendo pregunta 3

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    
+    const suma = a + b;
+    const mensaje = "The sum of " + a + " and " + b + " is " + suma + ".";
+    return [suma, mensaje];
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4,7);
@@ -26,7 +28,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    
+    const producto = a * b;
+    const mensaje = "The product of " + a + " and " + b + " is " + producto + ".";
+    return [producto, mensaje];
 }
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
